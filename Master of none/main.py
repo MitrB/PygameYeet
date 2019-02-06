c7dd34bb776d0b0395c2f4921dbda6d41c92103f
MitrB <MitrB@users.noreply.github.com>
2019-02-06 17:47:27 +0100

    Projectiles
    has some bugs

--- Master of none/main.py
+++ Master of none/main.py
@@ -1,6 +1,7 @@
 import pygame
 #import robot
-#import player
+import player as pl
+import classes as cl
 
 pygame.init()
 
@@ -8,46 +9,82 @@ pygame.init()
 window = (500, 500)
 win = pygame.display.set_mode(window)
 pygame.display.set_caption("Yeet sanctuary")
-
+# general pygame preload
 guy = pygame.image.load("Guy.png")
-run = True
 clock = pygame.time.Clock()
-
-x = 50
-y = 50
-vel = 5
+# the player character:
+character = pl.player(50,50, 64, 64)
+projectiles = []
 
 def redraw():
 
     win.fill((0,100,100))
-    win.blit(guy, (x,y))
+    win.blit(guy, (character.x, character.y))
+    for p in projectiles:
+        p.draw(win)
+
     pygame.display.update()
-    return
+    
 
 def movement():
-    global x
-    global y
 
     key = pygame.key.get_pressed()
     
-    if key[pygame.K_LEFT] and x > vel:
-        x -= vel
-    if key[pygame.K_RIGHT] and x < window[0] - vel - 32:
-        x += vel
-    if key[pygame.K_UP] and y > vel:
-        y -= vel
-    if key[pygame.K_DOWN]and y < window[1] - vel - 64:
-        y += vel  
+    if key[pygame.K_LEFT] and character.x > character.vel:
+        character.x -= character.vel
+        character.left = True
+    else:
+        character.left = False
+    if key[pygame.K_RIGHT] and character.x < window[0] - character.vel - character.width:
+        character.x += character.vel
+        character.right = True
+    else:
+        character.right = False
+    if key[pygame.K_UP] and character.y > character.vel:
+        character.y -= character.vel
+        character.up = True
+    else:
+        character.up = False
+    if key[pygame.K_DOWN]and character.y < window[1] - character.vel - character.height:
+        character.y += character.vel
+        character.down = True
+    else:
+        character.down = False
+    if key[pygame.K_SPACE]:
+        if character.left or character.up:
+            facing = -1
+        else:
+            facing = 1
+        if len(projectiles) < 5:
+            projectiles.append(cl.projectile(round(character.x + character.width//2), round(character.y + character.height//2), 2, (0,0,0), facing))
     return
 
+
+run = True
 while run:
-    clock.tick(30)
+    clock.tick(120)
     #pygame.time.delay(50)
+    print(projectiles)
+    for proj in projectiles:
+        if proj.x < window[0] and proj.x > 0 \
+        and proj.y < window[1] and proj.y > 0:
+            if character.right:
+                proj.x += proj.vel
+            if character.left:
+                proj.x -= proj.vel
+            if character.up:
+                proj.y -= proj.vel
+            if character.down:
+                proj.y += proj.vel
+        else:
+            projectiles.pop(projectiles.index(proj))
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
     
+
     movement()
     redraw()
 
--- Master of none/player.py
+++ Master of none/player.py
@@ -1,9 +1,26 @@
+import pygame
 class player(object):
     """docstring for player"""
-    def __init__(self, health, saturation, resources):
-        self.health = health
-        self.saturation = saturation
-        self.resources = resources
-    
 
-# BIG test #
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = 1
+
+        self.up = False
+        self.right = False
+        self.down = False
+        self.left = False
+        self.last = "right"
+
+    def draw(self, win):
+        win.blit(guy, (self.x, self.y))
+
+    def movementtrack(self, direction):
+        for i in [self.up, self.right, self.down, self.left]:
+            if direction != i:
+                i = False
+            else:
+                i = True   
